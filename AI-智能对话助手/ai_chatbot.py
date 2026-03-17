d4f73c7f1d3727f800e6c09dc3a16b34a3defa57
m0_52847932 <m0_52847932@noreply.gitcode.com>
2026-03-17 14:52:59 +0800

    feat:添加侧边栏交互

--- AI-智能对话助手/ai_chatbot.py
+++ AI-智能对话助手/ai_chatbot.py
@@ -4,7 +4,7 @@ from openai import OpenAI
 
 # 设置页面配置项
 st.set_page_config(
-    page_title="AI智能对话小助手",
+    page_title="你的AI网文书友",
     page_icon="👾",
     layout="wide",
     initial_sidebar_state="expanded",  #侧边栏状态
@@ -12,7 +12,7 @@ st.set_page_config(
 )
 
 # 大标题
-st.title("AI智能对话小助手")
+st.title("你的AI网文书友")
 
 # OpenAI client
 client = OpenAI(
@@ -21,14 +21,27 @@ client = OpenAI(
 
 # 系统提示词
 system_prompt = """
-你是一个深谙《龙族》全系列的小说粉，请用这个人设进行对话。
-"""
+        你叫%s, 现在是一个狂热的中文网络小说爱好者，请完全带入这个角色。
+        规则：
+            1. 匹配用户的语言
+            2. 可以经常性引用网文经典角色台词
+            3. 以朋友/书友的方式进行对话
+            4. 回复的内容，要充分体现你当前角色的性格特征
+        角色性格：
+            %s
+        你必须严格遵守以上规则来回复用户
+        """
 
 # 初始化聊天信息
 if "messages" not in st.session_state:
-    st.session_state.messages = [
-        {"role": "system", "content": system_prompt}
-    ]
+    st.session_state.messages = []
+
+# 初始化昵称
+if "nick_name" not in st.session_state:
+    st.session_state.nick_name = "小皮"
+# 初始化性格
+if "character" not in st.session_state:
+    st.session_state.character = "中二热血少年"
 
 # 展示聊天信息
 for message in st.session_state.messages:
@@ -36,8 +49,21 @@ for message in st.session_state.messages:
     if message["role"] != "system":
         st.chat_message(message["role"]).write(message["content"])
 
+# 左侧侧边栏
+with st.sidebar:
+    st.subheader("书友信息")
+    # 昵称输入框
+    nick_name = st.text_input("昵称", placeholder="请输入书友昵称", value=st.session_state.nick_name)
+    if nick_name:
+        st.session_state.nick_name = nick_name
+    # 性格输入框
+    character = st.text_area("性格", placeholder="请输入书友性格", value=st.session_state.character)
+    if character:
+        st.session_state.character = character
+
+
 # 聊天消息输入框
-prompt = st.chat_input("请输入你的问题")
+prompt = st.chat_input("请输入聊天内容")
 if prompt:
     st.chat_message("user").write(prompt)
     print("--------> 调用deepseek大模型，提示词：", prompt)
@@ -47,7 +73,10 @@ if prompt:
     # 调用大模型
     response = client.chat.completions.create(
         model="deepseek-chat",
-        messages=st.session_state.messages,
+        messages=[
+            {"role": "system", "content": system_prompt % (st.session_state.nick_name, st.session_state.character)},
+            *st.session_state.messages
+        ],
         stream=True
     )
 
@@ -63,6 +92,7 @@ if prompt:
             content = chunk.choices[0].delta.content
             full_response += content
             response_message.chat_message("assistant").write(full_response)
+    print("--------> 大模型返回的结果：", full_response)
 
 
     # 保存大模型返回结果
